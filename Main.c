5b0884db208c714af14ff4e2fbf60256d4f82aed
amnh30 <amn951753@gmail.com>
2024-09-22 18:59:52 +0300

    new

--- Main.c
+++ Main.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-    // Initialize the LCD
+
     MDIO_IntialPins();
 while(1){
-MDIO_u8SetPinValue(MDIO_PORTD,MDIO_PIN_6,MDIO_HIGH);
+MDIO_u8SetPinValue(MDIO_PORTD,MDIO_PIN_5,MDIO_HIGH);
 _delay_ms(200);
-MDIO_u8SetPinValue(MDIO_PORTD,MDIO_PIN_6,MDIO_LOW);
+MDIO_u8SetPinValue(MDIO_PORTD,MDIO_PIN_5,MDIO_LOW);
 _delay_ms(200);
 
 }
